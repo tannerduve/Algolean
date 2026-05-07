4dbb817045d28da579703f0acdef4dd9bb80308a
Arleee1 <keg9ve@virginia.edu>
2026-05-07 15:25:27 -0400

    add tests/examples for naive pattern search

--- AlgoleanTests.lean
+++ AlgoleanTests.lean
@@ -1,5 +1,6 @@
 module  -- shake: keep-all
 
 public import AlgoleanTests.CircuitExamples
+public import AlgoleanTests.NaivePatternSearchExamples
 public import AlgoleanTests.ProgExamples
 public import AlgoleanTests.QueryExamples
