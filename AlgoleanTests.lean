392bb74fcb4927913c7a6eb6ca3741a0ac572c46
Ethan Ermovick <61568556+Arleee1@users.noreply.github.com>
2026-05-10 04:17:07 -0400

    feat(KMP): Add tests for KMP (#51)

--- AlgoleanTests.lean
+++ AlgoleanTests.lean
@@ -1,5 +1,6 @@
 module  -- shake: keep-all
 
 public import AlgoleanTests.CircuitExamples
+public import AlgoleanTests.KMPExamples
 public import AlgoleanTests.ProgExamples
 public import AlgoleanTests.QueryExamples
