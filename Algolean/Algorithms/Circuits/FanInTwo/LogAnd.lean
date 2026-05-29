ba7cb3920d1674efa1decb2154f78f98ee2c138c
Bashar <bhcoding69@gmail.com>
2026-05-29 10:11:20 +0200

    authorship fix

--- Algolean/Algorithms/Circuits/FanInTwo/LogAnd.lean
+++ Algolean/Algorithms/Circuits/FanInTwo/LogAnd.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Shreyas Srinivas. All rights reserved.
+Copyright (c) 2026 Bashar Hamade. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Shreyas Srinivas
+Authors: Bashar Hamade
 -/
 
 module
